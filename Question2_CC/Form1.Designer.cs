c234e74ae632b79f411a37fced508567c3e0d0e9
MaryamBibi123 <mbibi2949@gmail.com>
2024-10-25 14:09:07 +0500

    Add project files.

---
+++ Question2_CC/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Question2_CC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.regNumberTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.regNumberLabel = new System.Windows.Forms.Label();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.favMovieLabel = new System.Windows.Forms.Label();
+            this.favMovieTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.Location = new System.Drawing.Point(112, 63);
+            this.headerLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(329, 37);
+            this.headerLabel.TabIndex = 0;
+            this.headerLabel.Text = "Password Generator";
+            // 
+            // regNumberTextBox
+            // 
+            this.regNumberTextBox.Location = new System.Drawing.Point(298, 162);
+            this.regNumberTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.regNumberTextBox.Name = "regNumberTextBox";
+            this.regNumberTextBox.Size = new System.Drawing.Size(294, 26);
+            this.regNumberTextBox.TabIndex = 1;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(298, 245);
+            this.firstNameTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(294, 26);
+            this.firstNameTextBox.TabIndex = 2;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(298, 325);
+            this.lastNameTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(294, 26);
+            this.lastNameTextBox.TabIndex = 3;
+            // 
+            // regNumberLabel
+            // 
+            this.regNumberLabel.AutoSize = true;
+            this.regNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.regNumberLabel.Location = new System.Drawing.Point(76, 162);
+            this.regNumberLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.regNumberLabel.Name = "regNumberLabel";
+            this.regNumberLabel.Size = new System.Drawing.Size(214, 29);
+            this.regNumberLabel.TabIndex = 4;
+            this.regNumberLabel.Text = "Enter your reg no";
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNameLabel.Location = new System.Drawing.Point(37, 242);
+            this.firstNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(253, 29);
+            this.firstNameLabel.TabIndex = 5;
+            this.firstNameLabel.Text = "Enter your first name";
+            // 
+            // favMovieLabel
+            // 
+            this.favMovieLabel.AutoSize = true;
+            this.favMovieLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.favMovieLabel.Location = new System.Drawing.Point(31, 383);
+            this.favMovieLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.favMovieLabel.Name = "favMovieLabel";
+            this.favMovieLabel.Size = new System.Drawing.Size(259, 29);
+            this.favMovieLabel.TabIndex = 6;
+            this.favMovieLabel.Text = "Enter favourite movie";
+            // 
+            // favMovieTextBox
+            // 
+            this.favMovieTextBox.Location = new System.Drawing.Point(298, 386);
+            this.favMovieTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.favMovieTextBox.Name = "favMovieTextBox";
+            this.favMovieTextBox.Size = new System.Drawing.Size(294, 26);
+            this.favMovieTextBox.TabIndex = 7;
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastNameLabel.Location = new System.Drawing.Point(39, 325);
+            this.lastNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(251, 29);
+            this.lastNameLabel.TabIndex = 8;
+            this.lastNameLabel.Text = "Enter your last name";
+            // 
+            // generateButton
+            // 
+            this.generateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateButton.Location = new System.Drawing.Point(201, 451);
+            this.generateButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(177, 35);
+            this.generateButton.TabIndex = 9;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(135, 512);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(354, 26);
+            this.passwordTextBox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 577);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.favMovieTextBox);
+            this.Controls.Add(this.favMovieLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.regNumberLabel);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.regNumberTextBox);
+            this.Controls.Add(this.headerLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.TextBox regNumberTextBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Label regNumberLabel;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label favMovieLabel;
+        private System.Windows.Forms.TextBox favMovieTextBox;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.TextBox passwordTextBox;
+    }
+}
---
+++ Question2_CC/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Question2_CC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            string registrationNumber = regNumberTextBox.Text; // Registration number input
+            string firstName = firstNameTextBox.Text; // First name input
+            string lastName = lastNameTextBox.Text; // Last name input
+            string favoriteMovie = favMovieTextBox.Text; // Favorite movie input
+
+            // Validate inputs
+            if (registrationNumber.Length < 2 || firstName.Length < 2 || lastName.Length < 2 || favoriteMovie.Length < 2)
+            {
+                MessageBox.Show("Please provide valid inputs.");
+                return;
+            }
+
+            // Extract required characters
+            string regDigits = Regex.Match(registrationNumber, @"\d{2}").Value; // First two digits
+            char secondFirstNameLetter = firstName.Length >= 2 ? firstName[1] : 'X'; // Second letter
+            char secondLastNameLetter = lastName.Length >= 2 ? lastName[1] : 'X'; // Second letter
+            string movieChars = favoriteMovie.Substring(0, 2); // First two letters of favorite movie
+
+            // Special characters, excluding '#'
+            string specialChars = "!$%&*@^";
+            Random rand = new Random();
+            char special1 = specialChars[rand.Next(specialChars.Length)];
+            char special2 = specialChars[rand.Next(specialChars.Length)];
+
+            // Combine parts to form the password
+            StringBuilder passwordBuilder = new StringBuilder();
+            passwordBuilder.Append(regDigits);
+            passwordBuilder.Append(secondFirstNameLetter);
+            passwordBuilder.Append(secondLastNameLetter);
+            passwordBuilder.Append(movieChars);
+            passwordBuilder.Append(special1);
+            passwordBuilder.Append(special2);
+
+            // Add random letters to meet the length of 14 characters
+            while (passwordBuilder.Length < 14)
+            {
+                char randChar = (char)rand.Next(65, 91); // Random uppercase letter
+                passwordBuilder.Append(randChar);
+            }
+
+            // Shuffle the password
+            string password = new string(passwordBuilder.ToString().OrderBy(x => rand.Next()).ToArray());
+
+            // Display the password
+            passwordTextBox.Text = password;
+        }
+
+      
+    }
+}
